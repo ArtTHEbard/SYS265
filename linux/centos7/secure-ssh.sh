0847c1fbae0beb8a885c2b66c60c3a9f2db3caf3
sjohnson <samuel.johnson01@mymail.champlain.edu>
2021-03-01 10:57:04 -0500

    ssh secure

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,5 @@
+#secure-ssh.sh
+#author sjohnson
+#creates a new ssh user using $1 parameter
+#adds a public from the local repo or curled from the remote repo
+#removes roots ability to ssh inecho "ALL YOUR CODE GOES HERE"
